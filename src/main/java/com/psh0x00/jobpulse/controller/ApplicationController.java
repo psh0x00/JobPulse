cc84bbf3f5216670643f7017bec8f828b612d8e9
psh0x00 <guilhermejorge@ipvc.pt>
2026-09-03 16:12:06 +0100

    Implement Status Transition Validation and Unit Tests

--- src/main/java/com/psh0x00/jobpulse/controller/ApplicationController.java
+++ src/main/java/com/psh0x00/jobpulse/controller/ApplicationController.java
@@ -4,6 +4,7 @@ package com.psh0x00.jobpulse.controller;
 import com.psh0x00.jobpulse.dto.ApplicationRequest;
 import com.psh0x00.jobpulse.dto.ApplicationResponse;
 import com.psh0x00.jobpulse.model.User;
+import com.psh0x00.jobpulse.model.enums.ApplicationStatus;
 import com.psh0x00.jobpulse.service.ApplicationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -34,5 +35,12 @@ public class ApplicationController {
     public ResponseEntity<List<ApplicationResponse>> getUserApplications(@AuthenticationPrincipal User currentUser){
         List<ApplicationResponse> userApplications = applicationService.getUserApplications(currentUser);
         return ResponseEntity.ok(userApplications);
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<ApplicationResponse> updateStatus(@PathVariable Long id, @RequestParam ApplicationStatus newStatus, @AuthenticationPrincipal User currentUser){
+
+        ApplicationResponse updatedApplication = applicationService.updateStatus(id, newStatus, currentUser);
+        return ResponseEntity.ok(updatedApplication);
     }
 }
