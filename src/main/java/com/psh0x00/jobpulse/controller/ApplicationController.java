68c735084f1f4c8b83a1745e1be58c152f6b0237
psh0x00 <98828750+psh0x00@users.noreply.github.com>
2026-09-03 17:20:58 +0100

    fix: leading slash in ApplicationController, JJWT version mismatch (#16)

--- src/main/java/com/psh0x00/jobpulse/controller/ApplicationController.java
+++ src/main/java/com/psh0x00/jobpulse/controller/ApplicationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/applications")
+@RequestMapping("/api/v1/applications")
 public class ApplicationController {
 
     private final ApplicationService applicationService;
