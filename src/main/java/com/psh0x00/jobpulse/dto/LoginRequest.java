f03cd7c27ed9eb3421c45c7b9c9cc435805c641d
psh0x00 <guilhermejorge@ipvc.pt>
2026-09-04 15:35:19 +0100

    feat: implement DTO input validation

--- src/main/java/com/psh0x00/jobpulse/dto/LoginRequest.java
+++ src/main/java/com/psh0x00/jobpulse/dto/LoginRequest.java
@@ -1,8 +1,17 @@
 package com.psh0x00.jobpulse.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequest {
 
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     public String getEmail() {
--- src/main/java/com/psh0x00/jobpulse/dto/RegisterRequest.java
+++ src/main/java/com/psh0x00/jobpulse/dto/RegisterRequest.java
@@ -1,9 +1,20 @@
 package com.psh0x00.jobpulse.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RegisterRequest {
 
+    @NotBlank
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
 
