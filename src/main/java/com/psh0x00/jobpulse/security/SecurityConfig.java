67481b715098ce34e81729671edd7cf6fcd8a9d7
psh0x00 <98828750+psh0x00@users.noreply.github.com>
2026-09-12 00:01:18 +0100

    feat: add spring boot actuator for health checks (Fixes #50 (#61)

--- src/main/java/com/psh0x00/jobpulse/security/SecurityConfig.java
+++ src/main/java/com/psh0x00/jobpulse/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                         "/api/v1/auth/**",
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
-                        "/swagger-ui.html"
+                        "/swagger-ui.html",
+                        "/actuator/**"
                 ).permitAll().anyRequest().authenticated())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
