4f92caeef06ab58d1bf8792f1009fc9f08f7f6ec
psh0x00 <98828750+psh0x00@users.noreply.github.com>
2026-09-11 21:52:45 +0100

    test: add testcontainers and integrity tests (Fixes #47) (#59)

--- src/test/java/com/psh0x00/jobpulse/JobpulseApplicationTests.java
+++ src/test/java/com/psh0x00/jobpulse/JobpulseApplicationTests.java
@@ -2,9 +2,41 @@ package com.psh0x00.jobpulse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class JobpulseApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+			.withDatabaseName("jobpulse_test")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+	}
+
+	@TestConfiguration
+	static class CacheTestConfig{
+		@Bean
+		public CacheManager cacheManager() {
+			return new ConcurrentMapCacheManager("dashboardStats");
+		}
+	}
 
 	@Test
 	void contextLoads() {
